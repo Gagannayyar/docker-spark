0916a520c9f07e23008b445ec9b9635f669c7896
Gagan Nayyar <gagan.nayyar@outlook.com>
2022-11-17 14:18:38 +0000

    Created CreateData class for data stream

--- scripts/data.py
+++ scripts/data.py
@@ -1,29 +1,68 @@
 import random
 import string
 import time
-
+import names
+import pandas as pd
 
 class CreateData:
-    """
-    creating a continious stream of data
-    """
 
-    def create_random_number():
+    "Create a small stream of dataframes for practice"
+
+    def __init__(self):
+        pass
+
+
+    def create_random_number(self): 
         number = random.randint(1, 1000)
         return number
 
-    def create_random_string():
-        strings  = res = ''.join(random.choices(string.ascii_uppercase +
-                             string.digits, k=random.randint(4, 12)))
+    def create_random_string(self):
+        strings  = res = ''.join(random.choices(string.ascii_uppercase +string.digits, k=random.randint(3,10)))
 
         return strings
+            
+    def create_names(self):
+        name = names.get_full_name()
+        return str(name)
+    
+    def create_data_dict(self,num):
+        """
+        Create a dictionary to be converted to dataframe in next step
+        Args:
+            num: The number of key, value pairs to be created
+        """
+        names = []
+        string = []
+        number = []
+        for item in range(0,num):
+            names.append(self.create_names())
+            string.append(self.create_random_string())
+            number.append(self.create_random_number())
 
-
-while True:
-    value = CreateData.create_random_number()
-    key = CreateData.create_random_string()
-    dictionary = {
-        key: value
-    }
-    print(dictionary)
-    time.sleep(2)
+            dicti = {
+                    "name": names,
+                    "code": string,
+                    "number": number
+                }
+        return dicti
+    
+    def create_dataframe(self,num):
+        dicti = self.create_data_dict(num)
+        df = pd.DataFrame(data=dicti)
+        return df
+    
+    def df_pertime(self,number_of_df,num,sleep_time=10):
+        """
+        Keep create a batch of dataframe with specific number of rows
+        args:
+            number_of_df: Number of dataframes to be created
+            num: Number of rows in dataframe
+            sleep_time: The time between consective dataframes in seconds 
+            (default is 10 seconds)
+        """
+        n = 1
+        while n <= number_of_df  :
+            df = self.create_dataframe(num)
+            n += 1
+            time.sleep(sleep_time)
+            return df
